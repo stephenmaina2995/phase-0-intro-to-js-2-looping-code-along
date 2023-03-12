8db91120e63071bccd24b56031c64a00e9238b82
stephenmaina2995 <stephenmaina2995@gmail.com>
2023-03-12 19:08:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  function writeCards(names, event) {
+    const birthdayMessage = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      birthdayMessage.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+  
+    return birthdayMessage;
+  }
+  
